357f2161d6769c0ef1085eba13f7df5cfe7ef566
Adisdzaky <adisdzakyrivai@gmail.com>
2025-12-24 19:56:19 +0700

    Update import path for globals.css in Layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-import './globals.css' // Baris ini harus ada!
+import '../styles/globals.css' // Baris ini harus ada!
 
 export default function RootLayout({ children }) {
   return (
