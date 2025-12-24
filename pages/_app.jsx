4259b674ec33801ca7ed716d4ec638cf76a1e62f
Adisdzaky <adisdzakyrivai@gmail.com>
2025-12-24 20:00:08 +0700

    Fix import statement for globals.css
    Ensure globals.css is imported correctly.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import Tap from "../components/Tap";
+import '../styles/globals.css' // Baris ini harus ada!
 
 export default function MyApp({ Component, pageProps }) {
   return (
