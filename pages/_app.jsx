ad03450a1f0bde7f3383a46950ccbe5c212ab7ee
Adisdzaky1 <adisdzakyrivai@gmail.com>
2025-12-24 19:29:18 +0700

    Tekan tombol

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import Tap from "../components/Tap";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Tap />
     </Layout>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export default {
+  content: [
+    "./components/*.{js,ts,jsx,tsx}",
+    "./pages/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
